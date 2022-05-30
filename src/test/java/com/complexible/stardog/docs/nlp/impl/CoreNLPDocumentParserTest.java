1d0f9957f066e9d23fa63243cb3be70bee691795
Zachary Whitley <zachary.whitley@gmail.com>
2022-05-30 13:32:25 -0400

    Updates to work with Stardog 7.9.1
    - Needed to use new Spans class
    - Move package because base class method is package private
    - Implement new abstrace base class method
    - Bump stardog version in gradle

--- src/test/java/com/complexible/stardog/docs/nlp/impl/CoreNLPDocumentParserTest.java
+++ src/test/java/com/complexible/stardog/docs/nlp/impl/CoreNLPDocumentParserTest.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.complexible.stardog.docs.corenlp;
+package com.complexible.stardog.docs.nlp.impl;
 
 import java.util.Arrays;
 
